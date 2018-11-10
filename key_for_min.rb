fcf7b3912659726913122c161ab095ae65eacc84
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-10 23:55:26 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-require 'pry'
-hash = {:blake => 500, :ashley => 2, :adam => 1}
+
 def key_for_min_value(name_hash)
   smallest = ""
   smallest_key=""
