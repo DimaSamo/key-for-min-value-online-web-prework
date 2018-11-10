be3a24aba127f985749d5019f7205ba801d91702
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-10 23:48:54 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   smallest = 0
   smallest_key=""
   if name_hash.empty?
-    nil
+    return nil
   else
     name_hash.each do |key, value|
       if value > smallest
